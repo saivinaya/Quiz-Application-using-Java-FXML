8a72d133c313a1141167ff2b655a6ffdd8bfcfdf
VinayaSaiD <VinayaSaiD@vinayas-mbp>
2016-11-10 07:53:58 +1030

    Adding questions package

---
+++ src/question/view/MultipleWithOneAnswerController.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package question.view;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ToggleGroup;
+
+/**
+ * FXML Controller class
+ *
+ * @author VinayaSaiD
+ */
+public class MultipleWithOneAnswerController implements Initializable {
+
+    @FXML
+    private ToggleGroup Choices;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
