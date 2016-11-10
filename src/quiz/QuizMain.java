e5fdd42ad4e635b9f26d37294c9ffb16abc436f1
VinayaSaiD <VinayaSaiD@vinayas-mbp>
2016-11-11 08:02:16 +1030

    Adding student.view, student dashboard, start test page and button functionality

--- src/quiz/QuizMain.java
+++ src/quiz/QuizMain.java
@@ -1,11 +1,19 @@
 package quiz;
 
 
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.fxml.JavaFXBuilderFactory;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import quiz.student.view.StartTestController;
+import quiz.student.view.StudentDashboardController;
 
 /**
  *
@@ -13,21 +21,65 @@ import javafx.stage.Stage;
  */
 public class QuizMain extends Application {
     
+    public Stage stage;
+    private User loggedUser;
+    private final double MINIMUM_WINDOW_WIDTH = 700.0;
+    private final double MINIMUM_WINDOW_HEIGHT = 700.0;
+    
+    
     @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
-        Scene scene = new Scene(root);
-        
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) {
+        try {
+            stage = primaryStage;
+            stage.setTitle("FXML Login Sample");
+            stage.setMinWidth(MINIMUM_WINDOW_WIDTH);
+            stage.setMinHeight(MINIMUM_WINDOW_HEIGHT);
+            gotoStudentDashboard();
+            primaryStage.show();
+        } catch (Exception ex) {
+            Logger.getLogger(QuizMain.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void gotoStudentDashboard() {
+        try {
+            StudentDashboardController profile = (StudentDashboardController) replaceSceneContent("student/view/StudentDashboard.fxml");
+            profile.setApp(this);
+        } catch (Exception ex) {
+            Logger.getLogger(QuizMain.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void gotoStartTest() {
+        try {
+            StartTestController profile = (StartTestController) replaceSceneContent("student/view/StartTest.fxml");
+            profile.setApp(this);
+        } catch (Exception ex) {
+            Logger.getLogger(QuizMain.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
+    public Initializable replaceSceneContent(String fxml) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        InputStream in = QuizMain.class.getResourceAsStream(fxml);
+        loader.setBuilderFactory(new JavaFXBuilderFactory());
+        loader.setLocation(QuizMain.class.getResource(fxml));
+        AnchorPane page;
+        try {
+            page = (AnchorPane) loader.load(in);
+        } finally {
+            in.close();
+        } 
+        Scene scene = new Scene(page, 800, 600);
+        stage.setScene(scene);
+        stage.sizeToScene();
+        return (Initializable) loader.getController();
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        launch(args);
+        Application.launch(args);
         
     }
     
---
+++ src/quiz/student/view/StartTestController.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quiz.student.view;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import quiz.QuizMain;
+
+/**
+ * FXML Controller class
+ *
+ * @author VinayaSaiD
+ */
+public class StartTestController implements Initializable {
+    private QuizMain application;
+    @FXML
+    Label errorMessage;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    public void setApp(QuizMain application){
+        this.application = application;
+    }
+
+    @FXML
+    private void goBackToStudentDashboard(ActionEvent event) {
+        if (application == null){
+            // We are running in isolated FXML, possibly in Scene Builder.
+            // NO-OP.
+            errorMessage.setText("Hello");
+        } 
+        else 
+        {
+            application.gotoStudentDashboard();
+        }
+    }
+    
+}
